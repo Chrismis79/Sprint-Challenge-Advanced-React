4df8f96fb55fed942cb5f6c5faf3fc018b5cf16d
Chrismis79 <chrismis79@gmail.com>
2019-11-01 13:26:47 -0500

    CDU fn buit and data is fetched

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+constructor(){
+  super();
+  this.state={
+    data: []
+  };
+}
+
+componentDidMount(){
+  axios.get(`http://localhost:5000/api/players`)
+  .then(res => {
+    this.setState(res.data);
+    this.setState = this.setState({data: res.data})
+  })
+  .catch(err => {
+    alert("There was a problem getting requested data.", err)
+  });
+}
+
+render(){
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Women's World Cup Players</h1>
+
       </header>
     </div>
   );
 }
-
+}
 export default App;
